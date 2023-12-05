1c36688b870a153ed8c5fd221ff481ca12bfb2f2
Fluffy <mrsargent00@gmail.com>
2023-12-05 16:24:25 -0500

    added code to test cirle and shapes.

---
+++ test/shapes.test.js
@@ -0,0 +1,54 @@
+const Shape = require("../lib/shape");
+
+const testCases = [
+  {
+    desc: "should throw an error if input is empty",
+    input: {},
+    expectedErr: new Error("Input cannot be empty"),
+  },
+  {
+    desc: "should throw error if logo text is longer than 3",
+    input: { logoName: "timmy" },
+    expectedErr: new Error("Logo text cannot be more than 3 characters"),
+  },
+  {
+    desc: "should throw error if input is not a valid CSS color",
+    input: { logoName: "Bob", textColour: "NotColour" },
+    expectedErr: new Error(
+      "Please enter a valid CSS color keyword or hex code"
+    ),
+  },
+  {
+    desc: "should throw an error if render() is called",
+    input: { logoName: "Bob", textColour: "green", bgColour: "purple" },
+    expectedErr: new Error("Child shapes must implement a render() method"),
+    shouldRender: true,
+  },
+  {
+    desc: "should add background color if it is a valid color",
+    input: { logoName: "Bob", textColour: "green", logoColour: "purple" },
+    expectedKey: "logoColour",
+    expectedValue: "purple",
+  },
+  {
+    desc: "should add text color if it is a valid color",
+    input: { logoName: "Bob", textColour: "red", logoColour: "purple" },
+    expectedKey: "textColour",
+    expectedValue: "red",
+  },
+];
+
+describe("Shape test suite", () => {
+  for (const tc of testCases) {
+    it(tc.desc, () => {
+      if (tc.shouldRender) {
+        expect(() => new Shape(tc.input).render()).toThrow(tc.expectedErr);
+      } else if (tc.expectedErr) {
+        expect(() => new Shape(tc.input)).toThrow(tc.expectedErr);
+      } else {
+        const shape = new Shape(tc.input);
+        expect(shape[tc.expectedKey]).toBe(tc.expectedValue);
+      }
+    });
+  }
+});
